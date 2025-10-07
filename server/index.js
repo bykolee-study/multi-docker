d4da23e3ba49709a3c853a5ba891227f8c7e5b3c
bykolee0 <bykolee1@gmail.com>
2025-10-07 20:54:43 +0900

    added k8s gke

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const pgClient = new Pool({
   database: keys.pgDatabase,
   password: keys.pgPassword,
   port: keys.pgPort,
+  ssl: false
   // ssl:
   //   process.env.NODE_ENV !== 'production'
   //     ? false
